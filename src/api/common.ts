16dc3a4f6447e0477dcb8bad8249d56912348a69
hamidomar000m <74457184+hamidomar000m@users.noreply.github.com>
2023-02-09 16:56:02 +0100

    atempted to write common/statistics request

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,1 +1,28 @@
-export const test = 'test';
+import type { FetchedStats, Stats } from "$types";
+import { fetchGetRequest, type GenericResponse } from "./api";
+
+type GetFetchedStatsResponse = {
+	stats: FetchedStats;
+};
+
+export type GetStatsResponse = {
+	stats: Stats;
+};
+
+export async function fetchStatistics(): Promise<GenericResponse<Stats>> {
+	const response = await fetchGetRequest<GetFetchedStatsResponse>('common/statistics');
+
+	let data: GetStatsResponse | undefined = undefined;
+	
+	if (response.data) {
+		data = {
+			stats: response.data.stats
+		};
+	}
+
+	return {
+		message: response.message,
+		error: response.error,
+		data: data?.stats
+	};
+}
