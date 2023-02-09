7c37df9c6a9ca7ab53906217e9fd8d6a26047155
hamidomar000m <74457184+hamidomar000m@users.noreply.github.com>
2023-02-09 16:08:37 +0100

    authors request fertig

--- src/api/authors.ts
+++ src/api/authors.ts
@@ -1,1 +1,190 @@
-export const test = 'test';
+import { 
+  convertArray, 
+  convertFetchedAuthorToAuthor, 
+  type Author, type AuthorType, 
+  type FetchedAuthor, 
+  type OrganizationAuthorProps, 
+  type PersonAuthorProps 
+} from "$types";
+
+import { 
+  fetchGetRequest, 
+  fetchPostRequest, 
+  fetchProtectedDeleteRequest, 
+  fetchProtectedPostRequest, 
+  fetchProtectedPutRequest, 
+  type GenericResponse 
+} from "./api";
+
+
+type GetFetchedAuthorResponse = {
+	author: FetchedAuthor;
+};
+
+export type GetAuthorResponse = {
+	author: Author;
+};
+
+export async function fetchAuthorById(id: string): Promise<GenericResponse<Author>> {
+	const response = await fetchGetRequest<GetFetchedAuthorResponse>('authors/author', {
+		id: id
+	});
+
+	let data: GetAuthorResponse | undefined = undefined;
+	
+	if (response.data) {
+		data = {
+			author: convertFetchedAuthorToAuthor(response.data.author)
+		};
+	}
+
+	return {
+		message: response.message,
+		error: response.error,
+		data: data?.author
+	};
+}
+
+export type CreateAuthorRequest = {
+	type: AuthorType;
+	personProperties?: PersonAuthorProps;
+	organizationProperties?: OrganizationAuthorProps;
+}
+
+export type CreateAuthorResponse = {
+  authorId: string;
+}
+
+export async function fetchCreateAuthor(
+	token: string,
+	request: CreateAuthorRequest
+): Promise<GenericResponse<string>> {
+	const response = await fetchProtectedPostRequest<CreateAuthorResponse>(
+		token,
+		'authors',
+		request
+	);
+
+	return {
+		message: response.message,
+		error: response.error,
+		data: response.data?.authorId
+	};
+}
+
+
+type AuthorFilter = {
+	approved?: boolean,
+	types?: AuthorType[],
+	firstName?: string,
+	lastName?: string,
+  organizationName?: string, 
+}
+
+export type GetAuthorPageRequest = {
+	page: number;
+	pageSize: number;
+	filter?:AuthorFilter;
+}
+
+type GetFetchedAuthorsResponse = {
+	authors: FetchedAuthor[];
+};
+
+export type GetAuthorPageResponse = {
+	authors: Author[];
+};
+
+export async function fetchAuthorPage(
+	request: GetAuthorPageRequest
+): Promise<GenericResponse<Author[]>> {
+	const response = await fetchPostRequest<GetFetchedAuthorsResponse>(
+		'authors/page',
+		request
+	);
+
+	let data: GetAuthorPageResponse | undefined = undefined;
+
+	if (response.data) {
+		data = {
+			authors: convertArray<FetchedAuthor, Author>(
+				response.data?.authors,
+				convertFetchedAuthorToAuthor
+			)
+		};
+	}
+
+	return {
+		message: response.message,
+		error: response.error,
+		data: data?.authors
+	};
+}
+
+
+export type GetAuthorPageCountRequest = {
+	pageSize: number;
+	filter?: AuthorFilter;
+}
+
+export type GetAuthorPageCountResponse = {
+	count: number;
+}
+
+export async function fetchAuthorPageCount(
+	request: GetAuthorPageCountRequest
+): Promise<GenericResponse<number>> {
+	const response = await fetchPostRequest<GetAuthorPageCountResponse>(
+		'authors/page_count',
+		request
+	);
+
+	return {
+		message: response.message,
+		error: response.error,
+		data: response.data?.count
+	};
+}
+
+export type ChangeAuthorRequest = {
+	id: string;
+	type: AuthorType;
+	personProperties?: PersonAuthorProps;
+	organizationProperties?: OrganizationAuthorProps;
+}
+
+export async function fetchChangeAuthor(
+	token: string,
+	request: ChangeAuthorRequest
+): Promise<GenericResponse<string>> {
+	const response = await fetchProtectedPutRequest<string>(
+		token,
+		'authors',
+		request
+	);
+
+	return {
+		message: response.message,
+		error: response.error,
+		data: response.data
+	};
+}
+
+export type GetDeleteAuthorResponse = {
+	sources?: string[];
+};
+
+export async function fetchDeleteAuthor(
+	token: string,
+	id: string
+): Promise<GenericResponse<string[] | undefined>> {
+	const response = await fetchProtectedDeleteRequest<GetDeleteAuthorResponse>(token, 'authors', {
+		id: id
+	});
+
+	return {
+		message: response.message,
+		error: response.error,
+		data: response.data?.sources
+	};
+}
