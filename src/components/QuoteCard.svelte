8ef3117e30601742266d88925fd1b3a914c955a7
Robert Witzke <robertwitzke.rw@gmail.com>
2023-02-15 09:24:57 +0100

    show username who has submitted the definition

--- src/components/QuoteCard.svelte
+++ src/components/QuoteCard.svelte
@@ -11,11 +11,14 @@
 </script>
 
 <div class="card w-[1/4] bg-base-100 shadow-xl">
-	<div class="card-body">
-		<div class="flex justify-between h-12 items-center">
+	<div class="card-body gap-6">
+		<div class="flex w-full justify-between h-12 items-center">
 			<div class="badge badge-secondary text-base w-fit h-fit">
 				{CategoryLabel[definition.category]}
 			</div>
+			<p class="text-end">
+				Submitted by: <span class="font-medium">{definition.submittedByName}</span>
+			</p>
 		</div>
 		<p class="flex italic text-xl text-justify items-center">"{definition.content}"</p>
 		<div class="flex flex-col justify-end items-end text-end">
